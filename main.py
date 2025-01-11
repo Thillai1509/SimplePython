5150071edc67361b720f82bccef1ca60a8c8aaef
Thillai1509 <112299204+Thillai1509@users.noreply.github.com>
2025-01-11 14:05:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-# main.py
+import os
+
 def main():
-    print("Hello, GitHub Actions!")
+    # Access the secret keys
+    secret_key_1 = os.getenv("SECRET_KEY_1")
+    secret_key_2 = os.getenv("SECRET_KEY_2")
+
+    # Use the keys as needed
+    print("Hi, Before Sending")
+
+    apiURL = f'https://api.telegram.org/bot{secret_key_1}/sendMessage'
+    message = "Hi, Welcome to Github Actions"
+    try:
+        response = requests.post(apiURL, json={'chat_id': secret_key_2, 'text': message})
+        print(response.text)
+    except Exception as e:
+        print(e)
+
+    print("Hi, Before Sending")
 
 if __name__ == "__main__":
     main()
